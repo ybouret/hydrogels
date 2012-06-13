eeaf5cf83996863da235e98a75183b0f46d2dbac
yann.bouret@gmail.com <yann.bouret@gmail.com@e8924d91-6aee-babe-a7f3-3138a32fa8dc>
2012-06-13 14:39:27 +0000

    added solution in core

--- trunk/src/gel1d/main.cpp
+++ trunk/src/gel1d/main.cpp
@@ -109,6 +109,26 @@ typedef layout1D        Layout;
 typedef workspace<Layout,double,rmesh> Workspace;
 
 
+static inline void workspace2solution( solution &s, const Workspace &w, unit_t i )
+{
+    component::iterator     q = s.begin();
+    for( size_t k = s.size; k>0; --k, ++q )
+    {
+        component &p = *q;
+        p.C = w[ p.name ].as<Array1D>()[ i ];
+    }
+}
+
+static inline void solution2workspace(  Workspace &w, const solution &s, unit_t i )
+{
+    component::const_iterator     q = s.begin();
+    for( size_t k = s.size; k>0; --k, ++q )
+    {
+        const component &p = *q;
+        w[ p.name ].as<Array1D>()[ i ] = p.C;
+    }
+}
+
 #define __GET_NUMBER(NAME) NAME( Lua::Config::Get<lua_Number>( L, #NAME ) )
 
 class Parameters 
@@ -136,6 +156,7 @@ public:
             const species &sp = **i;
             sim_fields.add<Array1D>( sp.name, true);
             sim_fields.add<Array1D>( sp.name+"_flux", false);
+            sim_fields.add<Array1D>( sp.name+"_incr", false);
         }
         sim_fields.add<Array1D>("x_half",false);
         sim_fields.add<Array1D>("ih",false);
@@ -171,8 +192,11 @@ public:
     
     Initializer ini_left;    //!< compute what is at left
     Initializer ini_right;   //!< compute what is at right
+    Initializer ini_core;    //!< compute what is inside core
     solution    sol_left;    //!< computed
     solution    sol_right;   //!< computed
+    solution    sol_core;    //!< computed
+    solution    sol_temp;    //!< for chemistry
     array_db   &adb;         //!< this
     Array1D    &x;
     Array1D    &x_half;
@@ -184,10 +208,13 @@ public:
     ChemSys(L,*this),
     Parameters(L,*this),
     Workspace( sim_layout, sim_ghosts, sim_fields),
-    ini_left( L, *this, "ini_left" ),
+    ini_left(  L, *this, "ini_left"  ),
     ini_right( L, *this, "ini_right" ),
+    ini_core(  L, *this, "ini_core"  ), 
     sol_left( *this ),
     sol_right( *this ),
+    sol_core( *this ),
+    sol_temp( *this ),
     adb( *this ),
     x( mesh.X() ),
     x_half(  adb["x_half" ].as<Array1D>() ),
@@ -201,9 +228,14 @@ public:
         sol_left.get( C );
         ini_right(*this,0.0);
         sol_right.get( C );
+        ini_core(*this,0.0);
+        sol_core.get( C );
         
-        std::cerr << "@left="  << std::endl << sol_left  << std::endl;
+        std::cerr << "@left =" << std::endl << sol_left  << std::endl;
         std::cerr << "@right=" << std::endl << sol_right << std::endl;
+        std::cerr << "@core =" << std::endl << sol_right << std::endl;
+
+        
         
         //----------------------------------------------------------------------
         // initialize physics
@@ -213,28 +245,41 @@ public:
         region1D<double>::type r(0,gel_length);
         mesh.regular_map_to(r, *this);
         
-        //-- compute auxiliary fields
+        //-- compute auxiliary field x_half
         for( unit_t i=0; i<ntop; ++i )
         {
             x_half[i] = 0.5 * ( x[i] + x[i+1] );
         }
         
-        //-- 
+        //-- compute volume size
         for( unit_t i=1; i < ntop; ++i )
         {
             ih[i] = 1.0 / (x_half[i]-x_half[i-1]);
         }
         
+        // compute distance between mesh vertices
         for(unit_t i=0; i <ntop; ++i )
         {
             ih_half[i]=1.0/(x[i+1] -x[i]);
         }
+        
+        // initialize
+        solution2workspace( *this, sol_left, 0);
+        for( unit_t i=1; i < ntop; ++i)
+            solution2workspace( *this, sol_core, i);
+        solution2workspace( *this, sol_right, ntop);
     }
     
     virtual ~Cell() throw()
     {
         
     }
+    
+    void compteDiff( double t, const string &id )
+    {
+        
+    }
+    
 };
 
 
