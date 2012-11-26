3aa43fa992b32c66e0cd53d4e54db7e2cf4df47d
yann.bouret <yann.bouret@gmail.com>
2012-11-26 16:38:08 +0000

    goind on

--- src/diff1d/library.cpp
+++ src/diff1d/library.cpp
@@ -1,11 +1,16 @@
 #include "library.hpp"
-
+#include "yocto/lua/lua-state.hpp"
 
 SpeciesData:: SpeciesData( double DiffusionCoeff ) throw() :
 D(DiffusionCoeff)
 {
     
 }
+
+SpeciesData:: ~SpeciesData() throw()
+{
+}
+
 
 
 Library:: ~Library() throw()
@@ -17,9 +22,13 @@ static inline
 void SpeciesDataCtor( species &sp, lua_State *L )
 {
     std::cerr << "-- Loading Data for " << sp.name << std::endl;
+    if( !lua_isnumber(L,-1) )
+        throw exception("Missing Diffusion Coefficicent for %s", sp.name.c_str());
+    sp.make<SpeciesData,double>( lua_tonumber(L, -1));
+    std::cerr << "D_{" << sp.name <<  "} = " << sp.get<SpeciesData>().D << std::endl;
 }
 
-Library:: Library( lua_State *L )
+Library:: Library( lua_State *L ) : library( sizeof(SpeciesData) )
 {
     _lua::species_ctor ctor( cfunctor2(SpeciesDataCtor) );
     _lua::load(L, *this, "species", &ctor);
