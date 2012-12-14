91b5a298f013f8b8fe223d1c90748301d4f594c8
yann.bouret <yann.bouret@gmail.com>
2012-12-14 16:41:35 +0000

    benchmarking...

--- src/benchmark/main.cpp
+++ src/benchmark/main.cpp
@@ -3,6 +3,9 @@
 #include "yocto/spade/workspace.hpp"
 #include "yocto/spade/rmesh.hpp"
 #include "yocto/math/types.hpp"
+#include "yocto/code/utils.hpp"
+#include "yocto/ios/ocstream.hpp"
+#include "yocto/math/fcn/functions.hpp"
 
 #include <iostream>
 
@@ -17,6 +20,10 @@ typedef workspace<Layout,rmesh,Real> Workspace;
 
 
 static const Real Kw = 1e-14;
+const double      h0 = 1e-2;
+const double      h1 = 1e-10;
+
+
 
 static size_t normalize( Real &h, Real &w, const Real ftol )
 {
@@ -33,6 +40,7 @@ static size_t normalize( Real &h, Real &w, const Real ftol )
         w += dw;
         if( Fabs(dh) > Fabs(ftol*h) ) continue;
         if( Fabs(dw) > Fabs(ftol*w) ) continue;
+        break;
     }
     return count;
 }
@@ -48,7 +56,6 @@ public:
     double                      ftol;
     const unit_t                imaxm1;
     const unit_t                imaxm2;
-    double                      alpha_max;
     const Layout                sim_layout;
     const ghosts_setup          no_ghost;
     fields_setup<Layout>        fields;
@@ -63,7 +70,6 @@ public:
     ftol(1e-5),
     imaxm1(imax-1),
     imaxm2(imax-2),
-    alpha_max(0.1),
     sim_layout(0,volumes),
     no_ghost(),
     fields(8)
@@ -80,7 +86,19 @@ public:
     }
     
     virtual ~Parameters() throw() {}
-        
+    
+    
+    
+    Real Kernel( Real t, Real x ) const
+    {
+        const Real args = (x/Sqrt(4*Dh*t));
+        const Real d0   = h0 - Kw/h0;
+        const Real d1   = h1 - Kw/h1;
+        const Real d    = d1 + (d0-d1) * qerfc(args);
+        return 0.5* ( d + Sqrt( d*d + 4.0 * Kw ) );
+    }
+    
+    
 private:
     YOCTO_DISABLE_COPY_AND_ASSIGN(Parameters);
 };
@@ -119,27 +137,38 @@ public:
     }
     
     
+    
+    
+    //--------------------------------------------------------------------------
+    //-- Compute fluxes, valid for any scheme
+    //--------------------------------------------------------------------------
     void compute_fluxes() throw()
     {
         for(unit_t i=0;i<imax;++i)
         {
             const unit_t ip = i+1;
-            Fh[i] = Dh * (h[ip] - h[i]) / dx;
-            Fw[i] = Dw * (w[ip] - w[i]) / dx;
+            Fh[i] = -Dh * (h[ip] - h[i]) / dx;
+            Fw[i] = -Dw * (w[ip] - w[i]) / dx;
         }
     }
     
+    //--------------------------------------------------------------------------
+    //-- Compute explicit fluxes
+    //--------------------------------------------------------------------------
     void compute_explicit_increases( const double dt ) throw()
     {
         for(unit_t i=1;i<imax;++i)
         {
             const unit_t im = i-1;
-            Ih[i] = - dt * (Fh[i] - Fh[im]);
-            Iw[i] = - dt * (Fw[i] - Fw[im]);
+            Ih[i] = - dt * (Fh[i] - Fh[im])/dx;
+            Iw[i] = - dt * (Fw[i] - Fw[im])/dx;
         }
         
     }
     
+    //--------------------------------------------------------------------------
+    //-- Compute constrained fluxes
+    //--------------------------------------------------------------------------
     void compute_constrained_increases( const double dt ) throw()
     {
         for(unit_t i=1;i<imax;++i)
@@ -148,17 +177,21 @@ public:
             const Real   _h = h[i];
             const Real   _w = w[i];
             const Real   _s = _h+_w;
-            const Real   gh = - dt * (Fh[i] - Fh[im]);
-            const Real   gw = - dt * (Fw[i] - Fw[im]);
+            const Real   gh = - dt * (Fh[i] - Fh[im])/dx;
+            const Real   gw = - dt * (Fw[i] - Fw[im])/dx;
             const Real   d  = (gh - gw)/_s;
             
             Ih[i] =   _h * d;
             Iw[i] = - _w * d;
         }
-        
-               
     }
     
+    
+    
+    
+    //--------------------------------------------------------------------------
+    //-- Updated explicit step with scaling
+    //--------------------------------------------------------------------------
     void update_explicit( const Real scaling ) throw()
     {
         for(unit_t i=1;i<imax;++i)
@@ -170,7 +203,10 @@ public:
         w[imax] = (4*w[imaxm1] - w[imaxm2])/3;
     }
     
-    void update_constraint( const Real scaling ) throw()
+    //--------------------------------------------------------------------------
+    //-- Updated constraint step with scaling
+    //--------------------------------------------------------------------------
+    void update_constrained( const Real scaling ) throw()
     {
         update_explicit(scaling);
         for(unit_t i=imax;i>0;--i)
@@ -178,12 +214,16 @@ public:
     }
     
     
+    
+    //--------------------------------------------------------------------------
+    //-- initialize to a pH step
+    //--------------------------------------------------------------------------
     void initialize() throw()
     {
-        h[0] = 1e-2;
+        h[0] = h0;
         w[0] = Kw/h[0];
         
-        h[1] = 1e-10;
+        h[1] = h1;
         w[1] = Kw/h[1];
         for( unit_t i=2;i<=imax;++i)
         {
@@ -192,11 +232,98 @@ public:
         }
     }
     
+    void find( Real &scaling, const Array &u, const Array &du ) throw()
+    {
+        for( unit_t i=1; i < imax; ++i )
+        {
+            const Real dU = du[i];
+            const Real U  = u[i];
+            if( dU < -U )
+            {
+                //std::cerr << "u[" << i << "]=" << U << " / dU=" << dU << std::endl;
+                //abort();
+                const Real factor = -U/dU; assert(factor>0);
+                if( scaling < 0 )
+                    scaling = factor;
+                else
+                {
+                    if(factor<scaling)
+                        scaling = factor;
+                }
+            }
+        }
+    }
+    
+    void step(Real t,
+              Real dt,
+              void (Simulation::*compute_increases)(Real),
+              void (Simulation::*update_fields)(Real) )
+    {
+        assert(compute_increases);
+        assert(update_fields);
+        
+        Real t_end = t + dt;
+        while(t<t_end)
+        {
+            dt = t_end - t;
+            compute_fluxes();
+            ((*this).*(compute_increases))(dt);
+            Real scaling = -1;
+            find(scaling,h,Ih);
+            find(scaling,w,Iw);
+            if( scaling > 0 )
+            {
+                std::cerr << "scaling=" << scaling << std::endl;
+                scaling *= 0.5;
+                dt *= scaling;
+                t += dt;
+                ((*this).*(update_fields))(scaling);
+                continue;
+            }
+            else
+            {
+                std::cerr << "full" << std::endl;
+                ((*this).*(update_fields))(1);
+                break; // was a full step
+            }
+        }
+    }
+    
+    void step_explicit(Real t, Real dt)
+    {
+        step(t,dt,&Simulation::compute_explicit_increases,&Simulation::update_explicit);
+    }
+    
+    void step_constrained(Real t, Real dt )
+    {
+        step(t,dt,&Simulation::compute_constrained_increases,&Simulation::update_constrained);
+    }
+    
+    
+    void save_profile( const string &fn, Real t ) const
+    {
+        ios::ocstream fp( fn, false);
+        for(unit_t i=0; i <= imax; ++i )
+        {
+            fp("%g %g %g %g\n", X[i], h[i], -log10(h[i]), -log10( Kernel(t,X[i])));
+        }
+    }
+    
 private:
     YOCTO_DISABLE_COPY_AND_ASSIGN(Simulation);
 };
 
 
+static inline
+double dt_round( double dt_max )
+{
+    //std::cerr << "dt_max=" << dt_max << std::endl;
+    const double dt_log = floor( log10(dt_max) );
+    //std::cerr << "dt_log=" << dt_log << std::endl;
+    const double dt_one = floor(dt_max * pow(10.0,-dt_log));
+    //std::cerr << "dt_one=" << dt_one << std::endl;
+    return dt_one * pow(10.0,dt_log);
+}
 
 
 int main(int argc, char *argv[])
@@ -204,9 +331,26 @@ int main(int argc, char *argv[])
     const char *progname = _vfs::get_base_name( argv[0]);
     try
     {
-        Simulation sim(100,1e-2);
+        const Real alpha = 0.1;
+        Simulation sim(1000,1e-2);
+        const Real dt_max = alpha * (sim.dx*sim.dx) / max_of(sim.Dh,sim.Dw);
+        const Real dt     = dt_round(dt_max);
+        std::cerr << "dt=" << dt << std::endl;
+        const Real t_run  = 4;
+        Real       t      = 0;
+        size_t iter_max = 1+ceil(t_run/dt);
+        std::cerr << "iter_max=" << iter_max << std::endl;
         
         sim.initialize();
+        sim.save_profile("h0.dat",0);
+        for(size_t iter=1;iter<=iter_max;++iter)
+        {
+            std::cerr << "iter=" << iter << std::endl;
+            sim.step_constrained(t,dt);
+            t = iter*dt;
+        }
+        sim.save_profile("h1.dat",t);
+        
         
         return 0;
     }
