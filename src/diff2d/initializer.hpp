696d9789cf16562c506ad18f04bf3afe7267f629
yann.bouret <yann.bouret@gmail.com>
2013-02-07 16:32:59 +0000

    still need init

---
+++ src/diff2d/initializer.hpp
@@ -0,0 +1,19 @@
+#ifndef INITIALIZER_INCLUDED
+#define INITIALIZER_INCLUDED 1
+
+#include "yocto/aqueous/initializer.hpp"
+#include "chemsys.hpp"
+
+//! initalizer with built-in electroneutrality
+class Initializer : public initializer
+{
+public:
+    explicit Initializer( const string &ini_name, const library &l, lua_State *L);
+    virtual ~Initializer() throw();
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Initializer);
+};
+
+#endif
+
