06060cf23684508cc73036ccf6fcafcf2b1ab6d9
yann.bouret@gmail.com <yann.bouret@gmail.com@e8924d91-6aee-babe-a7f3-3138a32fa8dc>
2013-11-27 14:07:03 +0000

    start new rdiff1d

---
+++ trunk/src/rdiff1d/main.cpp
@@ -0,0 +1,37 @@
+#include "collection.hpp"
+#include "yocto/fs/vfs.hpp"
+#include "yocto/exception.hpp"
+
+#include "yocto/lua/lua-state.hpp"
+#include "yocto/lua/lua-config.hpp"
+
+int  main(int argc, char *argv[] )
+{
+    const char *prog = vfs::get_base_name(argv[0]);
+    try
+    {
+        if( argc <= 1 )
+            throw exception("usage: %s config.lua",prog);
+        
+        std::cerr << "... loading file '" << argv[1] << "'" << std::endl;
+        Lua::State VM;
+        lua_State *L = VM();
+        Lua::Config::DoFile(L, argv[1]);
+        
+        Collection lib(L);
+        
+        return 0;
+    }
+    catch(const exception &e)
+    {
+        std::cerr << "*** in " << prog  << std::endl;
+        std::cerr << "*** " << e.what() << std::endl;
+        std::cerr << "*** " << e.when() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "*** in " << prog << std::endl;
+        std::cerr << "unhandled exception" << std::endl;
+    }
+    return 1;
+}
