f494ba1ad75b97b0694c3f9955dc4b3e789f63cf
yann.bouret@gmail.com <yann.bouret@gmail.com@e8924d91-6aee-babe-a7f3-3138a32fa8dc>
2013-11-28 14:31:50 +0000

    beginning of diffusion

--- trunk/src/rdiff1d/main.cpp
+++ trunk/src/rdiff1d/main.cpp
@@ -1,10 +1,16 @@
 #include "cell.hpp"
 
-#include "yocto/fs/vfs.hpp"
+#include "yocto/fs/local-fs.hpp"
 #include "yocto/exception.hpp"
 
 #include "yocto/lua/lua-state.hpp"
 #include "yocto/lua/lua-config.hpp"
+
+static inline
+bool is_curve( const vfs::entry &ep ) throw()
+{
+    return ep.has_extension("curve");
+}
 
 int  main(int argc, char *argv[] )
 {
@@ -14,11 +20,15 @@ int  main(int argc, char *argv[] )
         if( argc <= 1 )
             throw exception("usage: %s file1.lua [file2.lua ...]",prog);
         
+        vfs &fs = local_fs::instance();
+        fs.create_sub_dir("data");
+        fs.remove_files("data", is_curve);
+        
         //______________________________________________________________________
         //
         // Loading Lua resources
         //______________________________________________________________________
-
+        
         Lua::State VM;
         lua_State *L = VM();
         for(int i=1;i<argc;++i)
@@ -34,15 +44,26 @@ int  main(int argc, char *argv[] )
         
         Cell cell(L);
         
-        cell.initialize();
+        double dt = 0.1;
+        double t  = 0.0;
+        
+        cell.init_all();
+        cell.save_xy("data/v0.curve");
+        
+        for(int cycle=1;cycle<=200;++cycle)
+        {
+            cell.step(dt,t);
+            t = cycle * dt;
+            cell.save_xy("data/" + vformat("v%d.curve",cycle));
+        }
         
         return 0;
     }
     catch(const exception &e)
     {
         std::cerr << "*** in " << prog  << std::endl;
-        std::cerr << "*** " << e.what() << std::endl;
-        std::cerr << "*** " << e.when() << std::endl;
+        std::cerr << "*** "    << e.what() << std::endl;
+        std::cerr << "*** "    << e.when() << std::endl;
     }
     catch(...)
     {
