053ef95eec878cf6408817d44e5c9ef6fbd5c7a2
yann.bouret@gmail.com <yann.bouret@gmail.com@e8924d91-6aee-babe-a7f3-3138a32fa8dc>
2013-12-05 15:02:51 +0000

    need a better diffusion coefficient for heliantin

---
+++ trunk/src/rdiff1d/setup.lua
@@ -0,0 +1,12 @@
+
+volumes = 800;
+length  = 0.02; -- in meters
+
+alpha = 0.4;  -- dt Dmax/dx_min^2
+Tmax  = 60;   -- run time in seconds
+dt    = 0.01; -- required dt
+save  = 0.1;  -- in seconds
+
+search_front = 1;
+search_field = "H+";
+search_value = 10^(-5.5);
