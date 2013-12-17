c01959600ced3794c20c79abc4fb3ecf995bf02a
yann.bouret@gmail.com <yann.bouret@gmail.com@e8924d91-6aee-babe-a7f3-3138a32fa8dc>
2013-12-17 10:39:25 +0000

    ok for benchmarks

---
+++ trunk/benchmarks/launch.sh
@@ -0,0 +1,3 @@
+../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-3;"
+../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-5;"
+../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-7;"
