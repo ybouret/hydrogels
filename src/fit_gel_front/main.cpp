cd93bb661435e6e5384eaf84d122f62527d68849
yann.bouret <yann.bouret@gmail.com>
2014-02-17 14:18:44 +0000

    ok for fits, need to draw figures

--- src/fit_gel_front/main.cpp
+++ src/fit_gel_front/main.cpp
@@ -20,11 +20,11 @@ using namespace math;
 
 struct  Diffusion
 {
-    double compute( double t, const array<double> &a )
+    double Compute( double t, const array<double> &a )
     {
-        const double Df = a[1];
-        const double t0 = a[2];
-        return sqrt( max_of<double>(Df * (t-t0),0 ) );
+        const double Df = a[2];
+        const double t0 = a[1];
+        return Df * (t-t0);
     }
 };
 
@@ -36,84 +36,114 @@ int main( int argc, char *argv[] )
     const char *progname = vfs::get_base_name(argv[0]);
     try
     {
-        if(argc<=1)
+        if(argc<=2)
         {
-            throw exception("usage: %s file1.txt ...",progname);
+            throw exception("usage: %s file.txt t_mini", progname);
         }
         
-        for(int na=1;na<argc;++na)
+        const string filename = argv[1];
+        const double t_mini   = strconv::to<double>(argv[2],"t_mini");
+        
+        vector<double> t;
+        vector<double> x;
         {
-            const string filename = argv[na];
-            vector<double> t;
-            vector<double> x;
+            data_set<double> ds;
+            vector<double>   tt;
+            vector<double>   xx;
+            ds.use(1,tt);
+            ds.use(2,xx);
+            ios::icstream fp(filename);
+            ds.load(fp);
+            const size_t nt = tt.size();
+            t.reserve(nt);
+            x.reserve(nt);
+            
+            for(size_t i=1;i<=nt;++i)
             {
-                data_set<double> ds;
-                vector<double> tt;
-                vector<double> xx;
-                ds.use(1,tt);
-                ds.use(2,xx);
-                ios::icstream fp(filename);
-                ds.load(fp);
-                for(size_t i=1;i<=tt.size();++i)
+                if( xx[i] >= 0 && tt[i] >= t_mini)
                 {
-                    if( xx[i] >= 0 )
-                    {
-                        t.push_back(tt[i]);
-                        x.push_back(xx[i]);
-                        
-                    }
+                    t.push_back(tt[i]);
+                    x.push_back(xx[i]);
                 }
             }
+        }
+        
+        co_qsort(t, x);
+        const size_t n    = t.size();
+        const size_t nvar = 2;
+        
+        vector<double> aorg(nvar,0.0);
+        vector<double> aerr(nvar,0.0);
+        vector<bool>   used(nvar,true);
+        vector<double> x2(n,0.0);
+        vector<double> z(n,0.0);
+        
+        least_squares<double>::samples samples;
+        samples.append(t,x2,z);
+        for(size_t i=1;i<=n;++i)
+            x2[i] = __sqr(x[i]);
+        
+        least_squares<double>::sample &sample = *samples[1];
+        
+        double &Df = aorg[2];
+        double &t0 = aorg[1];
+        
+        double &Derr = aerr[2];
+        double &terr = aerr[1];
+        
+        sample.polynomial(aorg,used,aerr);
+        {
+            const double inter = aorg[1];
+            const double slope = aorg[2];
+            Df = slope;
+            t0 = -inter/slope;
+            std::cerr << "Df=" << Df << ", t0=" << t0 << std::endl;;
             
-            co_qsort(t,x);
-            const size_t   n = t.size();
-            vector<double> z(n,0);
-            
-            least_squares<double>::samples samples;
-            samples.append(t,x,z);
-            
-            least_squares<double> LSF;
-            
-            // variables
-            vector<double> a(2,0.0);
-            vector<bool>   used(2,true);
-            vector<double> aerr(2,0.0);
-            
-            //! diffusion coefficient estimation
-            double &Df = a[1];
-            double &t0 = a[2];
-            
+            ios::ocstream fp("output.dat", false);
+            for(size_t i=1;i<=n;++i)
             {
-                least_squares<double>::sample &sample = *samples[1];
-                numeric<double>::function      transf = cfunctor(__sqr);
-                sample.polynomial(a, used, aerr, &transf);
-                const double slope = a[2];
-                const double inter = a[1];
-                std::cerr << "slope=" << slope << ", inter=" << inter << std::endl;
-                Df =  slope;
-                t0 = -inter/Df;
-                std::cerr << "Df=" << Df << ", t0=" << t0 << std::endl;
+                const double dt = t[i] - t0;
+                fp("%g %g %g\n", t[i], x[i], sqrt(Df*dt) );
             }
-            
-            t0 = 0;
-            used[2] = false;
-            
-            Diffusion diff;
-            least_squares<double>::function F( &diff, & Diffusion::compute );
-          
-            if(LSF(F,samples,a,used,aerr,NULL) != least_squares_failure )
+        }
+        
+        least_squares<double> LSF;
+        LSF.h = 1e-5;
+        Diffusion diff;
+        least_squares<double>::function F(&diff, &Diffusion::Compute);
+        if( LSF(F,samples,aorg,used,aerr) != least_squares_failure )
+        {
+            std::cerr << "Df=" << Df << " +/-" << Derr << std::endl;
+            std::cerr << "t0=" << t0 << " +/-" << terr << std::endl;
+        
             {
-                std::cerr << "Df=" << Df << " +/-" << aerr[1] << std::endl;
-                std::cerr << "t0=" << t0 << " +/-" << aerr[2] << std::endl;
-                ios::ocstream fp("output.dat",false);
+                const string  output = filename + ".fit";
+                ios::ocstream fp(output,false);
+                fp("#t x fit\n");
                 for(size_t i=1;i<=n;++i)
                 {
-                    fp("%g %g %g\n", t[i], x[i], (z[i]));
+                    const double dt = t[i] - t0;
+                    z[i] = sqrt(max_of<double>(Df*dt,0));
+                    if(dt>=0)
+                    {
+                        fp("%g %g %g\n", dt, x[i], z[i]);
+                    }
                 }
+                
             }
             
-            
+            {
+                const string output = filename + ".log";
+                ios::ocstream fp(output,false);
+                fp("Df= %.8g +/- %.8g\n", Df, Derr);
+                fp("t0= %.8g +/- %.8g\n", t0, terr);
+                fp("corr= %.8g\n", compute_correlation(x, z));
+            }
+        
         }
+        
+        
+        
         
     }
     catch( const exception &e )
