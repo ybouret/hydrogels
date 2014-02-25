29e15f41f9aa5b0c866526940babb7da06e4e84d
yann.bouret <yann.bouret@gmail.com>
2014-02-25 16:26:23 +0000

    final benchs

---
+++ benchs/launch.sh
@@ -0,0 +1,3 @@
+nice -n 0 ../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-3;"
+nice -n 0 ../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-5;"
+nice -n 0 ../sdk/bin/benchmark ../src/benchmark/config.lua -f "ftol=1e-7;"
